c1e5e065a03255027eee79ab7d9f94493ca073e1
Joseph <joseph-tds@mail.ru>
2025-07-02 19:59:30 +0500

    uibuild

--- index.js
+++ index.js
@@ -11,95 +11,96 @@ app.use(requestLogger);
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Vonama!</h1>')
+  res.send('<h1>Hello Vonama!</h1>')
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
-    Note.findById(req.params.id)
-        .then(note => {
-            if(note){
-                res.json(note);
-            }else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error))    
+  Note.findById(req.params.id)
+    .then(note => {
+      if(note){
+        res.json(note);
+      }else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/notes/', (req, res, next) => {
-    const body = req.body;
+  const body = req.body;
 
-    if(!body.content){
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if(!body.content){
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    const note = new Note({        
-        content: body.content,
-        important: body.important || false
-    })
-    
-    note.save().then(savedNote => {
-        res.json(savedNote);
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
+
+  note.save().then(savedNote => {
+    res.json(savedNote);
+  })
     .catch(error => next(error));
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(204).end();
-        })
-        .catch(error => next(error));
+  Note.findByIdAndDelete(req.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const { content, important } = req.body;
+  const { content, important } = req.body;
 
-    Note.findById(req.params.id)
-        .then(note => {
-            if(!note){
-                return res.status(404).end();
-            }
-            note.content = content;
-            note.important = important;
+  Note.findById(req.params.id)
+    .then(note => {
+      if(!note){
+        return res.status(404).end();
+      }
+      note.content = content;
+      note.important = important;
 
-            return note.save().then(updatedNote => {
-                res.json(updatedNote);
-            })
-        })
-        .catch(error => next(error));
+      return note.save().then(updatedNote => {
+        res.json(updatedNote);
+      })
+    })
+    .catch(error => next(error));
 })
 
 app.get('/api/notes', (req, res) => {
-    Note.find({}).then(notes => {        
-        res.json(notes);
-    })
+  Note.find({}).then(notes => {
+    res.json(notes);
+  })
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'});
+  res.status(404).send({ error: 'unknown endpoint' });
 }
 app.use(unknownEndpoint);
 
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message);
+  console.error(error.message);
 
-    if(error.name === 'CastError'){
-        return res.status(400).send({ error: 'malformatted id'});
-    }else if(error.name === 'ValidationError'){
-        return res.status(400).json({error: error.message});
-    }
-    next(error)
+  if(error.name === 'CastError'){
+    return res.status(400).send({ error: 'malformatted id' });
+  }else if(error.name === 'ValidationError'){
+    return res.status(400).json({ error: error.message });
+  }
+  next(error)
 }
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 
--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -1,13 +1,14 @@
 const morgan = require('morgan');
-    
+
+// eslint-disable-next-line no-unused-vars
 morgan.token('body', (req, res) => {
-    if(req.method === 'POST'){
-        return JSON.stringify(req.body)
-    }else{
-        return ' '
-    }
+  if(req.method === 'POST'){
+    return JSON.stringify(req.body)
+  }else{
+    return ' '
+  }
 })
-const requestLogger = morgan(`:method :url :status :res[content-length] - :response-time ms :body`)
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
 
 module.exports = requestLogger;
